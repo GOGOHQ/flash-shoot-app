6ebd7f1c3939c078d2c535ba6e8c48df88c9fc29
GOGOHQ <657473650@qq.com>
2025-09-12 16:20:25 +0800

    1620

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,9 +1,9 @@
 class ApiConfig {
   // API 基础 URL - 使用您电脑的局域网 IP
-  static const String baseUrl = 'http://192.168.1.22:8080';
+  static const String baseUrl = 'https://burns-end-close-fioricet.trycloudflare.com';
   
   // 备用 URL（如果需要的话）
-  static const String backupBaseUrl = 'http://192.168.1.22:8080';
+  static const String backupBaseUrl = 'https://burns-end-close-fioricet.trycloudflare.com';
   
   // 超时设置
   static const Duration connectionTimeout = Duration(seconds: 30);
